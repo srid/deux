320f783496a9fcbcd2b32d9c88fc8ac61336530a
Sridhar Ratnakumar <srid@srid.ca>
2018-03-24 22:57:14 -0400

    Serve backend using scotty

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -1,12 +1,20 @@
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
+
+import Data.Monoid (mconcat)
+import qualified Data.Text.Lazy as T
+import Control.Monad.IO.Class (liftIO)
 
 import Dhall
 
+import Web.Scotty
+
 import Common
 
-main :: IO ()
-main = do
-  x <- input auto "../demo.dhall"
-  print (x :: Task)
+main = scotty 3001 $
+  get "/" $ do
+    -- TODO: use 3 layer cake
+    x :: Task <- liftIO $ input auto "../demo.dhall"
+    html $ T.pack $ show x
